942c6ce06626b47fac52ca0e8bdde395c0c1661d
BELILI Fahem <fahem.belili-ext@vinci-autoroutes.com>
2019-07-18 17:44:03 +0200

    initial commit of my plugin

---
+++ src/ios/MySuperCordovaPLugin.m
@@ -0,0 +1,28 @@
+/********* MySuperCordovaPLugin.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface MySuperCordovaPLugin : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation MySuperCordovaPLugin
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
